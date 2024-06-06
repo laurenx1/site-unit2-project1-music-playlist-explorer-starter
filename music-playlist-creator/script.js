4ff6f6c267c034025e3b3c8546ab5a5561c6d16a
Lauren Nia Pryor <lnpryor@meta.com>
2024-06-06 11:04:47 -0700

    modal working - unformatted

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,25 @@
 
+var playlists = document.getElementsByClassName("playlists")
+for (let i = 0; i < playlists.length; i++){
+    playlists[i].addEventListener("click", function() {
+        openModal();
+    })
+}
+
+
+// JavaScript for Opening and Closing the Modal
+var modal = document.getElementById("playlist-modal");
+var span = document.getElementsByClassName("close")[0];
+
+function openModal(card) {
+   modal.style.display = "block";
+}
+
+span.onclick = function() {
+   modal.style.display = "none";
+}
+window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+}
